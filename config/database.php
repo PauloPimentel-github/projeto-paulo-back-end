a873e6391f2a9efb184c706093aa9272bb116325
PauloPimentel <paulo.h.g.pimentel@gmail.com>
2018-07-22 23:31:22 -0300

    Atualizando database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'wiad5ra41q8129zn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'epc6e5ttiwv8nc2w'),
+            'username' => env('DB_USERNAME', 'a77ryve2nj0tdd0f'),
+            'password' => env('DB_PASSWORD', 'unhz737rawyky6yd'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
