058071bd49bc8923212ef41b8a3fc76791664f7d
PauloPimentel <paulo.h.g.pimentel@gmail.com>
2018-07-24 07:38:54 -0300

    Atualizando arquivos de Controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,42 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use LaravelLegends\PtBrValidator\ValidatorProvider;
 
 class CustomerController extends Controller
 {
-    //retorna todos os clientes cadastrados no banco
-    public function getCustomers()
-    {
-        return Customer::all();
+  //retorna todos os clientes cadastrados no banco
+  public function getCustomers()
+  {
+    $customer = Customer::all();
+
+    if (isset($customer)) {
+      return $customer;
+    }
+  }
+
+  //retorna apenas um cliente recuperado pelo campo id
+  public function getCustomer($customer_id)
+  {
+    return Customer::find($customer_id);
+  }
+
+  //cadastra um novo cliente no banco
+  public function createCustomer(Request $request)
+  {
+    Customer::create($request->all());
+    $json = array('success' => 'Usuário cadastrado com sucesso !!');
+    return $json;
+  }
+
+  //atualiza um cliente no banco
+  public function updateCustomer(Request $request, $id)
+  {
+    $customer = Customer::findOrFail($id);
+
+    if ($customer->update($request->all())) {
+      $json = array('status' => '1', 'success' => 'Cliente atualizado com sucesso !!');
+      return $json;
+    }
+  }
+
+  //deleta um cliente no banco
+  public function deleteCustomer(Request $request, $id)
+  {
+    $customer = Customer::findOrFail($id);
+
+    if ($customer->delete()) {
+      $json = array('success' => 'Cliente deletado com sucesso !!');
+      return $json;
     }
 
-    //retorna apenas um cliente recuperado pelo campo id
-    public function getCustomer($customer_id)
-    {
-        return Customer::find($customer_id);
-    }
-
-    //cadastra um novo cliente no banco
-    public function createCustomer(Request $request)
-    {
-        return Customer::create($request->all());
-    }
-
-    //atualiza um cliente no banco
-    public function updateCustomer(Request $request, $id)
-    {
-        $customer = Customer::findOrFail($id);
-        $customer->update($request->all());
-
-        return $customer;
-    }
-
-    //deleta um cliente no banco
-    public function deleteCustomer(Request $request, $id)
-    {
-        $customer = Customer::findOrFail($id);
-        $customer->delete();
-
-        return 204;
-    }
+    return $json;
+  }
 }
--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,48 +7,54 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //retorna todos os eventos cadastrados no banco
-    public function getEvents()
-    {
-        return Event::all();
+  //retorna todos os eventos cadastrados no banco
+  public function getEvents()
+  {
+    return Event::all();
+  }
+
+  //retorna apenas um eventos recuperado pelo campo id
+  public function getEvent($event_id)
+  {
+    return Event::find($event_id);
+  }
+
+  //retorna apenas a coluna de quantidade de mesas
+  public function getEventQuantTables($event_id)
+  {
+    return Event::select('events.event_quant_mesas')
+    ->where('events.event_id', '=', $event_id)
+    ->orderBy('events.event_quant_mesas', 'ASC')
+    ->get();
+  }
+
+  //cadastra um novo eventos no banco
+  public function createEvent(Request $request)
+  {
+    Event::create($request->all());
+    $json = array('success' => 'Evento cadastrado com sucesso !!');
+    return $json;
+  }
+
+  //atualiza um eventos no banco
+  public function updateEvent(Request $request, $id)
+  {
+    $event = Event::findOrFail($id);
+
+    if ($event->update($request->all())) {
+      $json = array('status' => '1', 'success' => 'Evento atualizado com sucesso !!');
+      return $json;
     }
+  }
 
-    //retorna apenas um eventos recuperado pelo campo id
-    public function getEvent($event_id)
-    {
-        return Event::find($event_id);
+  //deleta um eventos no banco
+  public function deleteEvent(Request $request, $id)
+  {
+    $event = Event::findOrFail($id);
+
+    if ($event->delete()) {
+      $json = array('success' => 'Evento deletado com sucesso !!');
+      return $json;
     }
-
-    //retorna apenas a coluna de quantidade de mesas
-    public function getEventQuantTables($event_id)
-    {
-        return Event::select('events.event_quant_mesas')
-        ->where('events.event_id', '=', $event_id)
-        ->orderBy('events.event_quant_mesas', 'ASC')
-        ->get();
-    }
-
-    //cadastra um novo eventos no banco
-    public function createEvent(Request $request)
-    {
-        return Event::create($request->all());
-    }
-
-    //atualiza um eventos no banco
-    public function updateEvent(Request $request, $id)
-    {
-        $event = Event::findOrFail($id);
-        $event->update($request->all());
-
-        return $event;
-    }
-
-    //deleta um eventos no banco
-    public function deleteEvent(Request $request, $id)
-    {
-        $event = Event::findOrFail($id);
-        $event->delete();
-
-        return 204;
-    }
+  }
 }
