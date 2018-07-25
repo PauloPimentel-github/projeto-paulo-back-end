ab9accb9cbeeb5f6fd02342c92052177bf3a3a6d
PauloPimentel <paulo.h.g.pimentel@gmail.com>
2018-07-25 06:56:46 -0300

    Atualizando arquivo de controller customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
-use LaravelLegends\PtBrValidator\ValidatorProvider;
+use Illuminate\Support\Facades\Validator;
 
 class CustomerController extends Controller
 {
@@ -27,17 +27,29 @@ class CustomerController extends Controller
   //cadastra um novo cliente no banco
   public function createCustomer(Request $request)
   {
-    Customer::create($request->all());
-    $json = array('success' => 'Usuário cadastrado com sucesso !!');
-    return $json;
+    $validator = Validator::make($request->all(), $this->validateForm($request));
+
+    if ($validator->fails()) {
+      return response($validator->errors()->all());
+    } else {
+      Customer::create($request->all());
+      $json = array('success' => 'Usuário cadastrado com sucesso !!');
+      return $json;
+    }
+
   }
 
   //atualiza um cliente no banco
   public function updateCustomer(Request $request, $id)
   {
-    $customer = Customer::findOrFail($id);
+    $validator = Validator::make($request->all(), $this->validateForm($request));
 
-    if ($customer->update($request->all())) {
+    if ($validator->fails()) {
+      return response($validator->errors()->all());
+    } else {
+      $customer = Customer::findOrFail($id);
+      $customer->update($request->all());
+
       $json = array('status' => '1', 'success' => 'Cliente atualizado com sucesso !!!');
       return $json;
     }
@@ -54,5 +66,18 @@ class CustomerController extends Controller
     }
 
     return $json;
+  }
+
+  //valida os campos do formulário
+  private function validateForm()
+  {
+    $rules = [
+      'customer_name' => 'required|string|max:100',
+      'customer_cpf' => 'required|string|unique:customers|max:11',
+      'customer_address' => 'required|string|max:100',
+      'customer_cep' => 'required|string|max:8'
+    ];
+
+    return $rules;
   }
 }
